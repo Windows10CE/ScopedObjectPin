65de3efbb0d5fbc04eae8bf1b8ca0e8b1e287609
Aaron Robinson <aaronrobin1234@gmail.com>
2024-01-10 15:32:56 -0600

    fix PtrAction placement

--- src/ScopedObjectPin/ScopedObjectPinner.cs
+++ src/ScopedObjectPin/ScopedObjectPinner.cs
@@ -2,7 +2,7 @@
 
 public static unsafe class ScopedObjectPinner
 {
-    public static void Pin(object o, IlHelpers.Helper.PtrAction callback)
+    public static void Pin(object o, IlHelpers.PtrAction callback)
     {
         IlHelpers.Helper.PinObject(o, callback);
     }
